748d1d99f9cbe620907dbe4ea3cf525d0d5e4d29
ETCasual <casualorient@gmail.com>
2022-02-22 02:21:55 +0800

    Initialized project

---
+++ src/pages/_app.tsx
@@ -0,0 +1,12 @@
+import { AppProps } from 'next/app';
+import { NextUIProvider } from '@nextui-org/react';
+
+const App = ({ Component, pageProps }: AppProps) => {
+  return (
+    <NextUIProvider>
+      <Component {...pageProps} />
+    </NextUIProvider>
+  );
+};
+
+export default App;
---
+++ src/pages/index.tsx
@@ -0,0 +1,7 @@
+import { Button } from '@nextui-org/react';
+
+const IndexPage = () => {
+  return <Button>Hi</Button>;
+};
+
+export default IndexPage;
